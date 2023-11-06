295e3eb42f5d1f5b0de9a284ce9c9ca984e540dc
jeongum <aldlswjddma@naver.com>
2023-11-06 21:30:22 +0900

    set spring security config

---
+++ src/main/kotlin/com/jeongum/springsecurity/security/SecurityConfig.kt
@@ -0,0 +1,26 @@
+package com.jeongum.springsecurity.security
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+class SecurityConfig{
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain =
+        http
+            .httpBasic { it.disable() }
+            .csrf { it.disable() }
+            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+            .authorizeHttpRequests {
+                it.requestMatchers("/auth/**", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                    .anyRequest().authenticated()
+            }
+            .build()
+
+    @Bean
+    fun passwordEncoder() = BCryptPasswordEncoder()
+}
